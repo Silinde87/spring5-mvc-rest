cbbb2526c384db58e1c8c8fde74ef82de0e6971e
Silinde87 <paurodriguezmolina@gmail.com>
2020-10-21 12:07:57 +0200

    Minor fix. Annotation not getting CategoryServiceImpl Bean.

--- src/main/java/springfamework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/springfamework/api/v1/mapper/CategoryMapper.java
@@ -1,15 +1,15 @@
 package springfamework.api.v1.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import springfamework.api.v1.model.CategoryDTO;
 import springfamework.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
-    CategoryDTO categoryToCategoryDTO(Category category);
 
+    CategoryDTO categoryToCategoryDTO(Category category);
 }
