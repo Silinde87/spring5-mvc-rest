b947b469586b4d3202a727a5aaa9a3efd05bfa2b
Silinde87 <paurodriguezmolina@gmail.com>
2020-10-21 10:43:18 +0200

    Category Service and tests created. Closes #2

--- src/main/java/springfamework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/springfamework/api/v1/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package springfamework.api.v1.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import springfamework.api.v1.model.CategoryDTO;
 import springfamework.domain.Category;
@@ -10,6 +11,7 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 
 }
---
+++ src/main/java/springfamework/services/CategoryService.java
@@ -0,0 +1,12 @@
+package springfamework.services;
+
+import springfamework.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
