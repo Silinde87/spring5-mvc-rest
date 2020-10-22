4f195081961c7612eb5085bdd4059266c662301d
Silinde87 <paurodriguezmolina@gmail.com>
2020-10-22 12:03:02 +0200

    Upgrade CategoryController to Spring4 @RestController annotation

--- src/main/java/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/springfamework/controllers/v1/CategoryController.java
@@ -3,14 +3,16 @@ package springfamework.controllers.v1;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import springfamework.api.v1.model.CategoryDTO;
 import springfamework.api.v1.model.CategoryListDTO;
 import springfamework.services.CategoryService;
 
-@Controller
+//I'm using this controller to test @RestController annotation, added at Spring 4.
+//It combines @Controller and @ResponseBody annotations.
+//This way we can keep code cleaner. Check CustomerController for earlier versions of Spring Controllers.
+
+@RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
 
@@ -21,16 +23,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getallCatetories(){
-
-        return new ResponseEntity<CategoryListDTO>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories(){
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName( @PathVariable String name){
-        return new ResponseEntity<CategoryDTO>(
-                categoryService.getCategoryByName(name), HttpStatus.OK
-        );
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name){
+        return categoryService.getCategoryByName(name);
     }
 }
